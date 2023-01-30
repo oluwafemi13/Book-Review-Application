c222e76b7d4716f22c50e847081de19ba54ec920
IKECHUKWU <sundayjabikem@gmail.com>
2023-01-30 21:58:08 +0100

    Created Entities

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User: IdentityUser
+    {
+        public virtual Guid UserId { get; set; }
+        public string Username { get; set; }
+    }
+}
